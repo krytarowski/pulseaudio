6a6521d5972e2f5d31dcb1c817c28961a0505f43
Kamil Rytarowski <n54@gmx.com>
2015-12-10 06:06:57 +0100

    netbsd: Replace nonstandard __WORDSIZE with a more portable solution
    There is no way to check CPU type in a portable way across ABIs.
    Checking for sizeof(void*) is reasonable since most platforms will
    report correct values. One exception is x32, but since it's halfbaked
    never finished and almost not needed any more - we can ignore it.
    
    The check is needed only to print a debug message, no functional
    change.
    
    This change fixes build on NetBSD.

--- src/tests/mult-s16-test.c
+++ src/tests/mult-s16-test.c
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <math.h>
+#include <limits.h>
 
 #include <pulse/rtclock.h>
 #include <pulsecore/random.h>
@@ -93,9 +94,9 @@ int main(int argc, char *argv[]) {
     if (!getenv("MAKE_CHECK"))
         pa_log_set_level(PA_LOG_DEBUG);
 
-#if __WORDSIZE == 64 || ((ULONG_MAX) > (UINT_MAX))
+#if (SIZEOF_VOIDP * CHAR_BIT) == 64
     pa_log_debug("This seems to be 64-bit code.");
-#elif  __WORDSIZE == 32
+#elif (SIZEOF_VOIDP * CHAR_BIT) == 32
     pa_log_debug("This seems to be 32-bit code.");
 #else
     pa_log_debug("Don't know if this is 32- or 64-bit code.");
